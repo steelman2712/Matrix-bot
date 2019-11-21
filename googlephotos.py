5bcf29b837397398e5ffc3648426fec2ffb9349e
steelman2712 <dreynolds96@hotmail.co.uk>
2019-11-21 20:14:36 +0000

    Added class gphotos

--- googlephotos.py
+++ googlephotos.py
@@ -7,29 +7,50 @@ import sqlite3
 
 
 ##Create list of albums
+class gphotos:
+    def __init__(self,service):
+        self.service = service
 
-def albumList(albums_to_search):
-    album_list = []
-    results = service.albums().list().execute()
-    for i in results["albums"]:
-        if i["title"] in albums_to_search:
-            album_list.append([i["title"],i["id"]])
-    return(album_list)
+    def albumList(self,albums_to_search):
+        album_list = []
+        results = self.service.albums().list().execute()
+        for i in results["albums"]:
+            if i["title"] in albums_to_search:
+                album_list.append([i["title"],i["id"]])
+        print(album_list)
+        return(album_list)
 
 
 
-#Gets photo info from albums
-def mediaFromAlbum(album_id):
-    photo = service.mediaItems().search(body={'albumId':album_id,'pageSize':'50'}).execute()
-    media = photo['mediaItems']
-    while 'nextPageToken' in photo:
-        nextPageToken = photo['nextPageToken']
-        photo = service.mediaItems().search(body={'albumId':album_id,'pageSize':'50','pageToken':nextPageToken}).execute()
-        media_temp = photo['mediaItems']
-        media = media+media_temp
-    return(media)
+    #Gets photo info from albums
+    def mediaFromAlbum(self,album_id):
+        photo = self.service.mediaItems().search(body={'albumId':album_id,'pageSize':'50'}).execute()
+        media = photo['mediaItems']
+        while 'nextPageToken' in photo:
+            nextPageToken = photo['nextPageToken']
+            photo = self.service.mediaItems().search(body={'albumId':album_id,'pageSize':'50','pageToken':nextPageToken}).execute()
+            media_temp = photo['mediaItems']
+            media = media+media_temp
+        return(media)
+    ##Gets photo url from id
+    def getGooglePhoto(self,photo_id):
+        photo_get = self.service.mediaItems().get(mediaItemId=photo_id).execute()
+        max_width = 1920
+        max_height = 1080
+        base_url = photo_get['baseUrl']+"=w"+str(max_width)+"-h"+str(max_height)
+        print(base_url)
+        local_file = wget.download(base_url, out="local_file.jpg")
+        local_file
 
-    ##Generate local database
+    def albumScrape(self):
+        results = self.service.albums().list().execute()
+        with open("albums.txt","w+") as outfile:
+            for i in results["albums"]:
+                outfile.write(i["title"]+"\n")
+
+
+
+##Generate local database
 def SQLImageList(directory,media,person_name):
     conn = sqlite3.connect(os.path.join(directory,"images.db"))
     c = conn.cursor()
@@ -57,16 +78,17 @@ def SQLImageList(directory,media,person_name):
 
 
 
-##Gets photo url from id
-def getGooglePhoto(photo_id):
-    photo_get = service.mediaItems().get(mediaItemId=photo_id).execute()
-    max_width = 1920
-    max_height = 1080
-    base_url = photo_get['baseUrl']+"=w"+str(max_width)+"-h"+str(max_height)
-    print(base_url)
-    local_file = wget.download(base_url, out="local_file.jpg")
-    local_file
 
+#Write album list to end of config file 
+def albumConfigWrite():
+    line_out = []
+    with open("albums.txt","r+") as infile:
+        for i in infile.read().splitlines():
+            line_out.append(i + " = " + i)
+    print(line_out)
+    with open("Matrix_Bot.ini","a") as configfile:
+        for i in line_out:
+            configfile.write(i+"\n")
 
 if __name__ == "__main__":
     SCOPES = 'https://www.googleapis.com/auth/photoslibrary.readonly'
@@ -76,5 +98,6 @@ if __name__ == "__main__":
         flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('photoslibrary', 'v1', http=creds.authorize(Http()))
+    service = gphotos(service)
 
 
