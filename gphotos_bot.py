ad41a9b522c39fd13bcf9d41f134c0fb37f162de
steelman2712 <dreynolds96@hotmail.co.uk>
2019-11-24 18:30:01 +0000

    Changed credentials creation location

--- gphotos_bot.py
+++ gphotos_bot.py
@@ -224,7 +224,7 @@ if __name__ == "__main__":
     store = file.Storage(os.path.join(cred_dir,'credentials.json'))
     creds = store.get()
     if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets(os.path.join('client_secret.json'), SCOPES)
+        flow = client.flow_from_clientsecrets(os.path.join(cred_dir,'client_secret.json'), SCOPES)
         creds = tools.run_flow(flow, store)
     service = build('photoslibrary', 'v1', http=creds.authorize(Http()))
     service = googlephotos.gphotos(service)      
